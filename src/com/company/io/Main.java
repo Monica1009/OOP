44a52afbddf8e6c3819c47a40e927f4bad438d09
DESKTOP-S3ELEHL\bucur <deca_monica@yahoo.com>
2021-03-18 19:24:21 +0200

    IO

---
+++ src/com/company/io/Main.java
@@ -0,0 +1,22 @@
+package com.company.io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+
+        Path path= Paths.get("");
+        List<String> text= new ArrayList<>();
+        text.add("Gabriel merge la piata");
+        text.add("Gabriel merge la magazin");
+        Files.write(path, text);
+    }
+
+}
---
+++ src/com/company/io/MainCitire.java
@@ -0,0 +1,19 @@
+package com.company.io;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class MainCitire {
+
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("C:/test/user.txt");
+        List<String> text= Files.readAllLines(path);
+        for (String line: text) {
+            System.out.println(line);
+
+        }
+    }
+}
